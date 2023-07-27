1d00eefa5270c76df3e58eb8e6c9078d1a3fdb8a
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-27 11:13:09 +0200

    create About section with content and cards

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import "./about.css"
+import ME from "../../assets/me-about.jpg"
+import {FaAward} from "react-icons/fa"
+import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
 
 const About = () => {
   return (
     <section id="about">
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-img">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Year Working Experience</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>Actively Looking For Clients</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Passionate web developer with strong technical skills seeking a
+            challenging and rewarding position as a Web Developer. Proven track
+            record in personnel development, customer service excellence, and
+            business transformation. Commited to delivering high-quality web
+            solutions through creativity, attention to detail, and
+            problem-solving abilities.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
