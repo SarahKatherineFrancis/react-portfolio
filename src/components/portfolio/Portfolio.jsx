073bb2e4685ea9d4e18183851cbdd83ab462c743
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-27 15:13:30 +0200

    style Portfolio section for responsive design

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./portfolio.css"
 import IMG1 from "../../assets/portfolio-1.png"
 import IMG2 from "../../assets/portfolio-2.png"
-import IMG3 from "../../assets/portfolio-3.png"
+import IMG3 from "../../assets/portfolio-3.jpg"
 import IMG4 from "../../assets/portfolio-4.png"
 import IMG5 from "../../assets/portfolio-5.png"
 import IMG6 from "../../assets/portfolio-6.png"
@@ -21,16 +21,18 @@ function Portfolio() {
 
           <h3>Elite Properties Group</h3>
 
-          <a
-          href="https://github.com/SarahKatherineFrancis/elite-properties-group"
-          className='btn' target='_blank'>
-            Github
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+            href="https://github.com/SarahKatherineFrancis/elite-properties-group"
+            className='btn' target='_blank'>
+              Github
+            </a>
 
-          <a href="https://warm-capybara-6049ad.netlify.app/"
-          className='btn btn-primary' target='_blank'>
-            Live Demo
-          </a>
+            <a href="https://warm-capybara-6049ad.netlify.app/"
+            className='btn btn-primary' target='_blank'>
+              Live Demo
+            </a>
+          </div>
         </article>
 
         <article className="portfolio__item">
@@ -40,16 +42,18 @@ function Portfolio() {
 
           <h3>Netflix Landing Page Clone</h3>
 
-          <a
-          href="https://github.com/SarahKatherineFrancis/netflix-clone"
-          className='btn' target='_blank'>
-            Github
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+            href="https://github.com/SarahKatherineFrancis/netflix-clone"
+            className='btn' target='_blank'>
+              Github
+            </a>
 
-          <a href="https://cosmic-brigadeiros-5d6374.netlify.app/"
-          className='btn btn-primary' target='_blank'>
-            Live Demo
-          </a>
+            <a href="https://cosmic-brigadeiros-5d6374.netlify.app/"
+            className='btn btn-primary' target='_blank'>
+              Live Demo
+            </a>
+          </div>
         </article>
 
         <article className="portfolio__item">
@@ -59,16 +63,18 @@ function Portfolio() {
 
           <h3>Journo Travel App</h3>
 
-          <a
-          href="https://github.com/SarahKatherineFrancis/journo"
-          className='btn' target='_blank'>
-            Github
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+            href="https://github.com/SarahKatherineFrancis/journo"
+            className='btn' target='_blank'>
+              Github
+            </a>
 
-          <a href="https://www.travel-journo.me/"
-          className='btn btn-primary' target='_blank'>
-            Live Demo
-          </a>
+            <a href="https://www.travel-journo.me/"
+            className='btn btn-primary' target='_blank'>
+              Live Demo
+            </a>
+          </div>
         </article>
 
         <article className="portfolio__item">
@@ -78,16 +84,18 @@ function Portfolio() {
 
           <h3>Furr Petsitting App</h3>
 
-          <a
-          href="https://github.com/SarahKatherineFrancis/furr"
-          className='btn' target='_blank'>
-            Github
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+            href="https://github.com/SarahKatherineFrancis/furr"
+            className='btn' target='_blank'>
+              Github
+            </a>
 
-          <a href="https://furr.herokuapp.com/"
-          className='btn btn-primary' target='_blank'>
-            Live Demo
-          </a>
+            <a href="https://furr.herokuapp.com/"
+            className='btn btn-primary' target='_blank'>
+              Live Demo
+            </a>
+          </div>
         </article>
 
         <article className="portfolio__item">
@@ -97,6 +105,7 @@ function Portfolio() {
 
           <h3>Lumo Dictionary App</h3>
 
+          <div className="portfolio__item-cta">
           <a
           href="https://github.com/SarahKatherineFrancis/react-dictionary-app"
           className='btn' target='_blank'>
@@ -107,6 +116,7 @@ function Portfolio() {
           className='btn btn-primary' target='_blank'>
             Live Demo
           </a>
+          </div>
         </article>
 
         <article className="portfolio__item">
@@ -116,16 +126,18 @@ function Portfolio() {
 
           <h3>Weather App</h3>
 
-          <a
-          href="https://github.com/SarahKatherineFrancis/react-weather-app"
-          className='btn' target='_blank'>
-            Github
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+            href="https://github.com/SarahKatherineFrancis/react-weather-app"
+            className='btn' target='_blank'>
+              Github
+            </a>
 
-          <a href="https://unruffled-agnesi-0b02b9.netlify.app/"
-          className='btn btn-primary' target='_blank'>
-            Live Demo
-          </a>
+            <a href="https://unruffled-agnesi-0b02b9.netlify.app/"
+            className='btn btn-primary' target='_blank'>
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
