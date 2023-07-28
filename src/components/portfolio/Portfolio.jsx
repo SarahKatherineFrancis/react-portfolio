7ee8036d3352a72373eea77e640316db23f2b9f3
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-28 11:22:42 +0200

    refactor Portfolio section using data array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,51 @@ import IMG4 from "../../assets/portfolio-4.png"
 import IMG5 from "../../assets/portfolio-5.png"
 import IMG6 from "../../assets/portfolio-6.png"
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Elite Properties Group",
+    github: "https://github.com/SarahKatherineFrancis/elite-properties-group",
+    demo: "https://warm-capybara-6049ad.netlify.app/"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Netflix Landing Page Clone",
+    github: "https://github.com/SarahKatherineFrancis/netflix-clone",
+    demo: "https://cosmic-brigadeiros-5d6374.netlify.app/"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Journo Travel App",
+    github: "https://github.com/SarahKatherineFrancis/journo",
+    demo: "https://www.travel-journo.me/"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Furr Petsitting App",
+    github: "https://github.com/SarahKatherineFrancis/furr",
+    demo: "https://furr.herokuapp.com/"
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Lumo Dictionary App",
+    github: "https://github.com/SarahKatherineFrancis/react-dictionary-app",
+    demo: "https://angry-booth-422a13.netlify.app/"
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Weather App",
+    github: "https://github.com/SarahKatherineFrancis/react-weather-app",
+    demo: "https://unruffled-agnesi-0b02b9.netlify.app/"
+  }
+]
+
 function Portfolio() {
   return (
     <section id="portfolio">
@@ -14,131 +59,29 @@ function Portfolio() {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG1} alt="Elite Properties Group" />
-          </div>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key= {id} className="portfolio__item">
+              <div className="portfolio__item-img">
+                <img src={image} alt={title} />
+              </div>
 
-          <h3>Elite Properties Group</h3>
+              <h3>{title}</h3>
 
-          <div className="portfolio__item-cta">
-            <a
-            href="https://github.com/SarahKatherineFrancis/elite-properties-group"
-            className='btn' target='_blank'>
-              Github
-            </a>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>
+                  Github
+                </a>
 
-            <a href="https://warm-capybara-6049ad.netlify.app/"
-            className='btn btn-primary' target='_blank'>
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG2} alt="Elite Properties Group" />
-          </div>
-
-          <h3>Netflix Landing Page Clone</h3>
-
-          <div className="portfolio__item-cta">
-            <a
-            href="https://github.com/SarahKatherineFrancis/netflix-clone"
-            className='btn' target='_blank'>
-              Github
-            </a>
-
-            <a href="https://cosmic-brigadeiros-5d6374.netlify.app/"
-            className='btn btn-primary' target='_blank'>
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG3} alt="Elite Properties Group" />
-          </div>
-
-          <h3>Journo Travel App</h3>
-
-          <div className="portfolio__item-cta">
-            <a
-            href="https://github.com/SarahKatherineFrancis/journo"
-            className='btn' target='_blank'>
-              Github
-            </a>
-
-            <a href="https://www.travel-journo.me/"
-            className='btn btn-primary' target='_blank'>
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG4} alt="Elite Properties Group" />
-          </div>
-
-          <h3>Furr Petsitting App</h3>
-
-          <div className="portfolio__item-cta">
-            <a
-            href="https://github.com/SarahKatherineFrancis/furr"
-            className='btn' target='_blank'>
-              Github
-            </a>
-
-            <a href="https://furr.herokuapp.com/"
-            className='btn btn-primary' target='_blank'>
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG5} alt="Elite Properties Group" />
-          </div>
-
-          <h3>Lumo Dictionary App</h3>
-
-          <div className="portfolio__item-cta">
-          <a
-          href="https://github.com/SarahKatherineFrancis/react-dictionary-app"
-          className='btn' target='_blank'>
-            Github
-          </a>
-
-          <a href="https://angry-booth-422a13.netlify.app/"
-          className='btn btn-primary' target='_blank'>
-            Live Demo
-          </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-img">
-            <img src={IMG6} alt="Elite Properties Group" />
-          </div>
-
-          <h3>Weather App</h3>
-
-          <div className="portfolio__item-cta">
-            <a
-            href="https://github.com/SarahKatherineFrancis/react-weather-app"
-            className='btn' target='_blank'>
-              Github
-            </a>
-
-            <a href="https://unruffled-agnesi-0b02b9.netlify.app/"
-            className='btn btn-primary' target='_blank'>
-              Live Demo
-            </a>
-          </div>
-        </article>
+                <a href={demo} className='btn btn-primary' target='_blank'>
+                  Live Demo
+                </a>
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
